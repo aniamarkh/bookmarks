ca24445ccd508b9a52ebf2574e1e3fce8dc242f0
aniamarkh <aniamarkh@gmail.com>
2023-01-24 23:49:48 +0300

    feat: add localstorage load and save

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 import { reactive } from "vue";
 import type { Bookmark, Store } from "./types";
+
+const localBookmarks: Bookmark[] = JSON.parse(localStorage.getItem("bookmarks") || '[]');
+
 export const store: Store = reactive(
   {
-    bookmarks: Array(),
+    bookmarks: localBookmarks,
     addBookmark(titleValue, urlValue, categoryValue) {
       let maxIdValue = this.bookmarks.reduce((acc, value: Bookmark) => {
         return (acc = acc > value.id ? acc : value.id);
       }, 0);
+
       let newBookmark: Bookmark = {
         id: maxIdValue + 1,
         title: titleValue,
@@ -14,6 +18,7 @@ export const store: Store = reactive(
         category: categoryValue,
       }
       this.bookmarks.push(newBookmark);
+      localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
     },
 
     deleteBookmark(id) {
@@ -21,6 +26,7 @@ export const store: Store = reactive(
 
       if (bookmarkToDelete >= 0) {
         this.bookmarks.splice(bookmarkToDelete, 1);
+        localStorage.setItem("bookmarks", JSON.stringify(this.bookmarks));
       }
     },
   }
