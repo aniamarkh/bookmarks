f7dfb137942352e78597413c7bfdba8bc345418f
aniamarkh <aniamarkh@gmail.com>
2023-01-24 15:28:29 +0300

    feat: move form to a separate component

---
+++ src/components/Form.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Ref } from "vue";
+import { store } from "../store";
+
+const input_title: Ref<string> = ref("");
+const input_url: Ref<string> = ref("");
+const input_category: Ref<string> = ref("");
+
+const isInvalidInputs = (): boolean => {
+  const isEmpty: boolean =
+    input_title.value.trim() === "" ||
+    input_url.value.trim() === "" ||
+    input_category.value.trim() === "";
+  let invalidUrl: boolean;
+
+  try {
+    new URL(input_url.value);
+    invalidUrl = false;
+  } catch (err) {
+    invalidUrl = true;
+  }
+
+  return isEmpty || invalidUrl;
+};
+
+const addBookmark = (): void => {
+  let maxIdValue = store.reduce((acc, value) => {
+    return (acc = acc > value.id ? acc : value.id);
+  }, 0);
+
+  store.push({
+    id: maxIdValue + 1,
+    title: input_title.value,
+    url: input_url.value,
+    category: input_category.value,
+  });
+
+  input_title.value = "";
+  input_url.value = "";
+  input_category.value = "";
+};
+</script>
+
+<template>
+  <div class="form-wrapper">
+    <form class="bookmark_form" @submit.prevent="addBookmark">
+      <h4>Add new link</h4>
+      <input type="text" placeholder="title" v-model="input_title" />
+      <input type="url" placeholder="url" v-model="input_url" />
+      <input type="text" placeholder="category" v-model="input_category" />
+      <input
+        type="submit"
+        value="Add a bookmark"
+        :disabled="isInvalidInputs()"
+      />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.bookmark_form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
--- src/components/LinkList.vue
+++ src/components/LinkList.vue
@@ -1,45 +1,5 @@
 <script setup lang="ts">
 import { store } from "../store";
-import { ref } from "vue";
-import type { Ref } from "vue";
-
-const input_title: Ref<string> = ref("");
-const input_url: Ref<string> = ref("");
-const input_category: Ref<string> = ref("");
-
-const isInvalidInputs = (): boolean => {
-  const isEmpty: boolean =
-    input_title.value.trim() === "" ||
-    input_url.value.trim() === "" ||
-    input_category.value.trim() === "";
-  let invalidUrl: boolean;
-
-  try {
-    new URL(input_url.value);
-    invalidUrl = false;
-  } catch (err) {
-    invalidUrl = true;
-  }
-
-  return isEmpty || invalidUrl;
-};
-
-const addBookmark = (): void => {
-  let maxIdValue = store.reduce((acc, value) => {
-    return (acc = acc > value.id ? acc : value.id);
-  }, 0);
-
-  store.push({
-    id: maxIdValue + 1,
-    title: input_title.value,
-    url: input_url.value,
-    category: input_category.value,
-  });
-
-  input_title.value = "";
-  input_url.value = "";
-  input_category.value = "";
-};
 
 const deleteBookmark = (id: number): void => {
   const bookmarkToDelete = store.findIndex((bookmark) => bookmark.id === id);
@@ -59,37 +19,33 @@ const filterByCategory = (category: string) => {
 </script>
 
 <template>
-  <div class="form-wrapper">
-    <form class="bookmark_form" @submit.prevent="addBookmark">
-      <h4>Add new link</h4>
-      <input type="text" placeholder="title" v-model="input_title" />
-      <input type="url" placeholder="url" v-model="input_url" />
-      <input type="text" placeholder="category" v-model="input_category" />
-      <input
-        type="submit"
-        value="Add a bookmark"
-        :disabled="isInvalidInputs()"
-      />
-    </form>
-  </div>
-  <div
-    class="categories-wrapper"
-    v-for="(category, index) in formCategories()"
-    :key="index"
-  >
-    <h4>{{ category }}</h4>
-    <ul>
-      <li v-for="bookmark in filterByCategory(category)" :key="bookmark.id">
-        <a :href="bookmark.url" target="_blank">{{ bookmark.title }}</a>
-        <button @click="deleteBookmark(bookmark.id)">×</button>
-        <!--todo: <button @click="editBookmark(bookmark.id)">edit</button> -->
-      </li>
-    </ul>
+  <div class="categories-wrapper">
+    <div
+      class="category"
+      v-for="(category, index) in formCategories()"
+      :key="index"
+    >
+      <h4>{{ category }}</h4>
+      <ul>
+        <li v-for="bookmark in filterByCategory(category)" :key="bookmark.id">
+          <a :href="bookmark.url" target="_blank">{{ bookmark.title }}</a>
+          <button @click="deleteBookmark(bookmark.id)">×</button>
+          <!--todo: <button @click="editBookmark(bookmark.id)">edit</button> -->
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .categories-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 20px;
+  margin-top: 20px;
+}
+.category {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
@@ -98,13 +54,7 @@ const filterByCategory = (category: string) => {
   border-radius: 10px;
   gap: 20px;
   width: 200px;
-  justify-content: space-between;
   text-align: center;
-}
-.bookmark_form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
 }
 
 ul {
