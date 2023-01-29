651c5cfea83166ffc785e435bbbc82f452955642
aniamarkh <aniamarkh@gmail.com>
2023-01-29 22:06:29 +0300

    tests: add bookmark, get bookmark for category, delete bookmark

--- src/store.test.ts
+++ src/store.test.ts
@@ -6,17 +6,17 @@ import { store as _store } from './store'
 const store = _store
 
 test('Add category', () => {
-  store.addCategory('Swimming')
+  store.addCategory('Swimming');
   expect(store.categories).toEqual(
     [{
       id: 1,
       title: 'Swimming',
       bookmarks: []
     }]
-  )
-  expect(store.bookmarks).toEqual([])
+  );
+  expect(store.bookmarks).toEqual([]);
 
-  store.addCategory('Bowling')
+  store.addCategory('Bowling');
   expect(store.categories).toEqual(
     [{
       id: 1,
@@ -28,7 +28,79 @@ test('Add category', () => {
       title: 'Bowling',
       bookmarks: [],
     }]
-  )
-  expect(store.bookmarks).toEqual([])
-})
+  );
+  expect(store.bookmarks).toEqual([]);
+});
 
+test('Add bookmark', () => {
+  store.addBookmark('My pool', 'http://pool.com/', 1);
+  store.addBookmark('Bowling', 'http://bowling.com/', 2);
+
+  expect(store.categories).toEqual(
+    [{
+      id: 1,
+      title: 'Swimming',
+      bookmarks: [1],
+    },
+    {
+      id: 2,
+      title: 'Bowling',
+      bookmarks: [2],
+    }]
+  );
+  expect(store.bookmarks).toEqual(
+    [
+      {
+        id: 1,
+        title: 'My pool',
+        url: 'http://pool.com/',
+      },
+      {
+        id: 2,
+        title: 'Bowling',
+        url: 'http://bowling.com/',
+      }
+    ]
+  );
+});
+
+test('Get bookmarks for category', () => {
+  let result = store.getBookmarksFor(1);
+  expect(result).toEqual([{
+    id: 1,
+    title: 'My pool',
+    url: 'http://pool.com/',
+  }]);
+  result = store.getBookmarksFor(2);
+  expect(result).toEqual([{
+    id: 2,
+    title: 'Bowling',
+    url: 'http://bowling.com/',
+  }]);
+
+});
+
+test('Delete bookmark', () => {
+  store.deleteBookmark(1, 1);
+  expect(store.categories).toEqual(
+    [{
+      id: 1,
+      title: 'Swimming',
+      bookmarks: [],
+    },
+    {
+      id: 2,
+      title: 'Bowling',
+      bookmarks: [2],
+    }]
+  );
+  expect(store.bookmarks).toEqual(
+    [
+      {
+        id: 2,
+        title: 'Bowling',
+        url: 'http://bowling.com/',
+      }
+    ]
+  );
+});
