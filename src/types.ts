b468ef8a9d3b27b2006ba65296bd89011058687e
aniamarkh <aniamarkh@gmail.com>
2023-01-30 21:30:57 +0300

    tests: rewrite previous tests & add tests for findMaxId, findNodeById, findParentNodeById store methods

--- src/types.ts
+++ src/types.ts
@@ -18,11 +18,12 @@ export interface Data {
 
 export interface Store {
   data: Category;
+  addCategory(title: string): void;
+  addBookmark(nodeId: number, title: string, url: string): void;
+  deleteBookmark(bookmarkId: number): void;
   saveToLocalStore(): void;
   loadFromLocalStore(): void;
   findMaxId(node: Category): number;
   findNodeById(node: Category | Bookmark, id: number): Category | Bookmark | null;
-  addCategory(title: string): void;
-  addBookmark(nodeId: number, title: string, url: string): void;
-  deleteBookmark(bookmarkId: number): void;
+  findParentNodeById(node: Category | Bookmark, id: number): Category | null;
 }
