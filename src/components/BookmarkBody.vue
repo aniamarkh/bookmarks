78c86959ac407776459ce43a71c8fd4b2b023e8e
aniamarkh <aniamarkh@gmail.com>
2023-02-20 18:40:11 +0300

    fix: reactivity when edit bookmark

--- src/components/BookmarkBody.vue
+++ src/components/BookmarkBody.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { Ref } from "vue";
+import { ref, toRef } from "vue";
+import type {Bookmark} from "../types";
 import { store } from "../store";
 
 const props = defineProps({
-  bookmark: { type: Object, required: true },
+  bookmark: { type: Object as () => Bookmark, required: true },
 });
+
+const bookmarkProp: Bookmark = toRef(props, "bookmark").value;
 
 const showEditForm = ref(false);
 
-const input_newTitle: Ref<string> = ref(props.bookmark.title);
-const input_newUrl: Ref<string> = ref(props.bookmark.url);
-
 const isInvalidInputs = (): boolean => {
-  const isEmpty: boolean =
-    input_newTitle.value.trim() === "" || input_newUrl.value.trim() === "";
+  const isEmpty: boolean = bookmarkProp.url.trim() === "";
   let invalidUrl: boolean;
 
   try {
-    new URL(input_newUrl.value);
+    new URL(bookmarkProp.url);
     invalidUrl = false;
   } catch (err) {
     invalidUrl = true;
@@ -28,7 +26,13 @@ const isInvalidInputs = (): boolean => {
 };
 
 const onSubmit = (): void => {
-  store.editBookmark(props.bookmark.id, input_newTitle.value, input_newUrl.value);
+  let title: string;
+  if (bookmarkProp.title === "") {
+    store.updateBookmarkTitle(bookmarkProp.url, bookmarkProp.id);
+  } else {
+    title = bookmarkProp.title;
+  }
+  store.editBookmark(bookmarkProp.id, title, bookmarkProp.url);
   showEditForm.value = false;
 }
 </script>
@@ -38,15 +42,15 @@ const onSubmit = (): void => {
     <img class="favicon" v-bind:src="bookmark.favicon" />
     <a :href="bookmark.url" target="_blank">{{ bookmark.title }}</a>
     <button class="bookmark-btn" @click="showEditForm = !showEditForm">
-      <img src="./assets/edit.svg" alt="edit" />
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M4.583 15.667h.896l8.083-8.084-.895-.916-8.084 8.083Zm10.771-8.625-2.125-2.167.833-.833q.271-.271.615-.271t.635.271l.896.916q.25.25.23.636-.021.385-.271.635Zm-.542.541-8.958 8.938H3.729v-2.146l8.938-8.917Zm-1.687-.458-.458-.458.895.916Z"/></svg>
     </button>
     <button class="bookmark-btn" @click="store.deleteNode(bookmark.id)">
-      <img src="./assets/delete.svg" alt="delete" />
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M7.042 16.042q-.459 0-.782-.323-.322-.323-.322-.802V5.542h-.98v-.604h3.063v-.792h3.958v.792h3.083v.604h-.979v9.375q0 .479-.323.802-.322.323-.802.323Zm6.437-10.5H6.542v9.375q0 .229.146.375.145.146.354.146h5.916q.188 0 .354-.157.167-.156.167-.364Zm-4.75 8.416h.604V7.021h-.604Zm1.938 0h.604V7.021h-.604ZM6.542 5.542v9.896-.521Z"/></svg>
     </button>
   </div>
   <form class="bookmark-edit_form" v-if="showEditForm" @submit.prevent="onSubmit">
-    <input type="text" placeholder="new title" v-model="input_newTitle"/>
-    <input type="url" placeholder="new url" v-model="input_newUrl"/>
+    <input type="text" placeholder="new title" v-model="bookmarkProp.title"/>
+    <input type="url" placeholder="new url" v-model="bookmarkProp.url"/>
     <div class="bookmark-form--btns">
       <button class="submit-btn" :disabled="isInvalidInputs()" @click.prevent="onSubmit"></button>
       <button class="cancel-btn" @click="!showEditForm"></button>
