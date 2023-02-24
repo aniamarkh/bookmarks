971492f1ce51815a35b866acaa270cad69daf7c7
aniamarkh <aniamarkh@gmail.com>
2023-02-24 14:29:42 +0300

    fix: page height, horizontal scroll

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,17 @@ import App from "./App.vue";
 import "./style.css"
 
 createApp(App).mount("#app");
+
+const scrollable = document.querySelector(".categories-wrapper")
+if (scrollable) {
+  window.addEventListener("wheel", event => {
+    if (event.deltaY > 0) {
+      console.log("scroll");
+      scrollable.scrollLeft += 50;
+    } else {
+      scrollable.scrollLeft -= 50;
+      console.log("scroll");
+
+    }
+  });
+}
